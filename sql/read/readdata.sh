bd598cba1b2725bef6e087217382fd300588e403
Robins Tharakan <tharakan@gmail.com>
2016-12-11 23:07:56 +1100

    Now all available queries are tried (sent) on Postgres. They are painfully slow, but they are getting sent one by one... Haven't seen them finish :) .. because I don't have the patience yet

--- sql/read/readdata.sh
+++ sql/read/readdata.sh
@@ -1,3 +1,7 @@
-../../tools/dsqgen -template ../../query_templates/query99.tpl -dir .  -dialect ../../query_templates/postgres -scale 1
+for i in `seq 1 99`
+do
+  echo Running Query ${i}
+  ../../tools/dsqgen -template ../../query_templates/query${i}.tpl -dir .  -dialect ../../query_templates/postgres -scale 1
+  psql -h localhost -U postgres -f query_0.sql -o output/ans${i}.out tpc2
+done
 
-psql -h localhost -U postgres -f query_0.sql tpc2
