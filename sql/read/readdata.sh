43e8acadc8cc278503750aa34ffe82682030e49e
Robins Tharakan <tharakan@gmail.com>
2017-06-24 00:54:37 +1000

    Correct the query_template path. Temporarily remove -h localhost as well

--- sql/read/readdata.sh
+++ sql/read/readdata.sh
@@ -1,10 +1,10 @@
-for i in `seq 1 99`
+for i in `seq 1 1`
 do
   echo Running Query ${i}
   /home/robins/projects/tpcds-kit/tools/dsqgen \
-    -template ../query_templates/query${i}.tpl \
+    -template ../../query_templates/query${i}.tpl \
     -dir .  \
-    -dialect ../query_templates/postgres -scale 1
-  psql -h localhost -U postgres -f query_0.sql -d tpc2 2>&1 > /home/robins/projects/tpcds-kit/sql/read/output/ans${i}.out
+    -dialect ../../query_templates/postgres -scale 1
+  psql -U postgres -f query_0.sql -d tpc2 2>&1 > /home/robins/projects/tpcds-kit/sql/read/output/ans${i}.out
 done
 
